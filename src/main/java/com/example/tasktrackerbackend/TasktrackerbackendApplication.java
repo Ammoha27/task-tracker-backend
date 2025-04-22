b166f724125d0b2826400830db9d24627968f88b
Amir Mohamed <amir.mohamed@comfone.com>
2025-04-22 11:16:57 +0200

    first commit

---
+++ src/main/java/com/example/tasktrackerbackend/TasktrackerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasktrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasktrackerbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasktrackerbackendApplication.class, args);
+    }
+
+}
